350c0cf536775f9de1b3f4b72f39581f29f20971
Ulrich Knorr <Maverick@ulrichs-macbook-pro.local>
2019-03-20 21:22:42 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ulrich Knorr on 20.03.19.
+//  Copyright © 2019 Ulrich Knorr. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
